2e3061173ce91dda348c152311941e8227b49f04
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-02-15 15:13:23 +0530

    routing update

--- api/router/train.router.js
+++ api/router/train.router.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import {bookTicket, searchTrain, test, trainAddressList, trainUpload} from '../controller/train.controller.js'
+import { searchTrain, test, trainAddressList, trainUpload, getAvailableSeats, bookTicket, payment, getPnr,} from '../controller/train.controller.js'
 
 const router = express.Router()
-router.get('/test',test)
-router.post('/upload',trainUpload)
-router.get('/addresslist',trainAddressList)
-router.post('/search',searchTrain)
+router.get('/test', test)
+router.post('/upload', trainUpload)
+router.get('/address-list', trainAddressList)
+router.get('/search', searchTrain)
+router.get('/available-seats', getAvailableSeats)
 router.post('/book-ticket',bookTicket)
+router.post('/book-ticket/payment',payment)
+router.get('/get-my-booking',getPnr)
+
 export default router
