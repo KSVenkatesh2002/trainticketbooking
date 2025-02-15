d8ffc823d168318e5548fc315e17a6c500d1b4ab
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-02-15 15:14:18 +0530

    home and search separated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,234 +1,59 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { setTrainsList} from '../redux/slices/trainSlice';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardList, faCodeCommit, faSearch, faTrain } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
-    const currentDate = new Date()
-    const min = currentDate.toISOString().split('T')[0]
-
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-
-    const [source, setSource] = useState("");
-    const [destination, setDestination] = useState("");
-    const [sourceSuggestions, setSourceSuggestions] = useState([]);
-    const [destinationSuggestions, setDestinationSuggestions] = useState([]);
-    const [focusedSource,setFocusedSource] = useState(false)
-    const [focusedDestination,setFocusedDestination] = useState(false)
-    const [searchFormData, setSearchFormData] = useState({
-        source:'',
-        destination:'',
-        date:min,
-        classes:'sleeper'
-    })
-    const [error, setError] = useState(false)
-
-    const [finalAddress, setFinalAddress] = useState([]);
-
-    // useEffect(()=>{
-    //     console.log('\n\n\n',searchFormData)
-    //     console.log('sourceSuggestions :',sourceSuggestions)
-    //     console.log('destinationSuggestions :',destinationSuggestions)
-    //     console.log('final address :',finalAddress)
-    // },[searchFormData, sourceSuggestions, destinationSuggestions, finalAddress])
-
-    const onFocus = (type) => {
-        if(type==='source')   setFocusedSource(true)
-        else    setFocusedDestination(true) 
-    }
-    const onBlur = (type) => {
-        setTimeout(() => {
-            if (type === "source") setFocusedSource(false);
-            else setFocusedDestination(false);
-        }, 500); // Delay allows the click event to trigger first
-    }
-
-    const handleOnChange = (e) => {
-        setError('')
-        setSearchFormData({...searchFormData, [e.target.name]:e.target.value})
-    }
-
-    const handleSumbit = async (e) => {
-        setError('')
-        e.preventDefault();
-        try{
-            const res = await fetch('/api/train/search',{
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                },
-                body: JSON.stringify({
-                    sourceStation:finalAddress[0],
-                    destinationStation:finalAddress[1],
-                    date:searchFormData.date,
-                    classes:searchFormData.classes
-                }),
-            })
-            const data = await res.json()
-            console.log(data)
-            if(data.success)   {
-                dispatch(setTrainsList({
-                    trains:data.filteredTrains,
-                    source,
-                    destination,
-                    date: searchFormData.date
-                }))
-                navigate("/train-list", {
-                    state: { data }})}
-            else   setError(data.message)
-        
-        }catch(e){
-            setError(e.message)
-        }
-    }
-
-    // Fetch station suggestions
-    const fetchSuggestions = async (query, type) => {
-        if (query==='') {
-            if (type === "source") setSourceSuggestions([]);
-            else setDestinationSuggestions([]);
-            return
-        };
-
-        const response = await fetch(`/api/train/addresslist?address=${query}`);
-        const data = await response.json();
-
-        if (type === "source") setSourceSuggestions(data);
-        else setDestinationSuggestions(data);
-    };
-
+    const location = useLocation();
+    const [nav,setNav] = useState('')
+    const route = location.pathname.split('/')[location.pathname.split('/').length-1]
+    useEffect(()=>{
+        setNav(route)
+    },[route])
+    
+//
     return (
-        <div className="flex flex-col items-center ">
+        <div className="flex flex-col items-center h-full min-h-screen w-full">
 
-            {/* intro */}
-            <div className='w-screen my-10 text-center grow'>
-                <h1 className='font-bold'>Welcome to Train Ticket Booking center! 🚆</h1>
-                <p className=''>Ready to embark on your next adventure? Whether you're planning a quick getaway or a long journey, we're here to make your travel experience seamless and enjoyable. Book your tickets with ease, explore diverse destinations, and get ready to create unforgettable memories. Let's get started!</p>
-                <h2>Your journey begins here.</h2>
-                <p>Safe travels, and happy journey! 😊✨</p>
-            </div>
-            <hr className='border w-screen mx-4' />
 
-            {/* form */}
-            <div className='flex flex-col md:flex-row items-center w-screen'>
-                <form onSubmit={(e)=>handleSumbit(e)} className="flex flex-col w-screen max-w-[700px] md:max-w-screen p-4 grow">
-                    <h1 className="b text-5xl my-4">BOOK TICKET</h1>
+            <div className='w-full h-[10vh] md:h-[12vh] min-h-[50px] mt-5 flex justify-evenly items-center'>
+                <div className='flex justify-evenly items-center bg-gray-100 h-9/10 rounded-2xl w-1/5 text-center hover:underline'>
+                    <Link to='/search' className={`text-orange-200 ${nav === 'search' && 'text-orange-600' } font-semibold`}>
+                        <FontAwesomeIcon icon={faSearch} className={`hover:text-4xl text-3xl block md:hidden`} /><br />
+                        <span className="hidden md:inline">Search</span>
+                    </Link>
+                </div>
 
-                    {/* row1 */}
-                    <div className="flex flex-col justify-between md:mb-5  md:flex-row">
+                <div className='flex justify-evenly items-center bg-gray-100 h-9/10 rounded-2xl w-1/5 text-center hover:underline'>
+                    <Link to='/pnr-status' className={`text-orange-200 ${nav === 'pnr-status' && 'text-orange-600' } font-semibold `} >
+                        <FontAwesomeIcon icon={faCodeCommit} className="hover:text-4xl text-3xl block md:hidden" /><br />
+                        <span className="hidden md:inline">PNR status</span>
+                    </Link>
+                </div>
 
-                        {/* From */}
-                        <div className="w-1/2">
-                            <label className="text-sm">From</label><br />
-                            <input 
-                                type="text" 
-                                value={source}
-                                name='source'
-                                onChange={(e) => {
-                                    setSource(e.target.value);
-                                    fetchSuggestions(e.target.value, "source");}}
-                                onFocus={()=>onFocus('source')} onBlur={()=>onBlur('source')}
-                                className=" border-2  focus:border-b-4 rounded-lg  h-12 text-2xl" 
-                            /> 
+                <div className='flex justify-evenly items-center bg-gray-100 h-9/10 rounded-2xl w-1/5 text-center hover:underline'>
+                    <Link to='/train-info' className={`text-orange-200 ${nav === 'train-info' && 'text-orange-600' } font-semibold `}>
+                        <FontAwesomeIcon icon={faTrain} className="hover:text-4xl text-3xl bloctext-3xl k md:hidden" /><br />
+                        <span className="hidden md:inline">Train info</span>
+                    </Link>
+                </div>
 
-                            { focusedSource && 
-                            <ul>
-                                {sourceSuggestions.map((station) => (
-                                <li key={station._id} 
-                                    onClick={()=> {
-                                        setSource(station.name);
-                                        setFinalAddress([station,finalAddress[1]])}}>
-                                    {station.name}
-                                </li>
-                                ))}
-                            </ul>}
-                        </div>
-
-                        <span></span>
-
-                        {/* To */}
-                        <div className="w-1/2">
-                            <label className="text-sm">To</label><br />
-                            <input 
-                                type="text" 
-                                name='destination'
-                                className="border-2  focus:border-b-4 rounded-lg h-12 text-2xl" 
-                                value={destination}
-                                onFocus={()=>onFocus('destination')} 
-                                onBlur={()=>onBlur('destination')}
-                                onChange={(e) => {
-                                    setDestination(e.target.value);
-                                    fetchSuggestions(e.target.value, "destination");}}/>
-
-                            {focusedDestination &&
-                            <ul>
-                                {destinationSuggestions.map((station) => (
-                                <li key={station._id} 
-                                    onClick={()=> { 
-                                        setDestination(station.name);
-                                        setFinalAddress([finalAddress[0],station])}}>
-                                    {station.name}
-                                </li>
-                                ))}
-                            </ul>}
-                        </div>
-                        {/* Show suggestions for destination */}
-                    </div>
-
-                    {/* row2 */}
-                    <div className="flex flex-col justify-between md:flex-row mb-10">
-
-                        {/* Date */}
-                        <div className="w-1/2">
-                            <label className="text-sm">Date</label><br />
-                            <input 
-                                type="date"
-                                name='date'
-                                min={min} 
-                                value={searchFormData.date}
-                                onChange={(e)=>handleOnChange(e)}
-                                className="border-2  focus:border-b-4 rounded-lg h-12  text-2xl" />
-                        </div>
-
-                        {/* Classess */}
-                        <div className="w-1/2">
-                            <label className="text-sm">Classses</label><br />
-                            <select 
-                                type="string" 
-                                name='classes'
-                                value={searchFormData.classes} 
-                                onChange={(e)=>handleOnChange(e)}
-                                className="border-2  focus:border-b-4 rounded-lg h-12  text-2xl"
-                            >
-                                <option value="sleeper">SLEEPER</option>
-                                <option value="ac 2 tier">AC 2 Tier</option>
-                                <option value="ac 3 tier">AC 3 Tier</option>
-                                <option value="chair car">CHAIR CAR</option>
-                                <option value="executive">EXECUTIVE</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    {/* row3 */}
-                    <button
-                        className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-                    >SEARCH</button>
-                    <h3 className='text-red-500 font-bold self-center text-2xl'>{error}</h3>
-
-                </form>
-
-                <hr className='border w-screen md:hidden mb-5 mt-5' />
-                {/* other links */}
-                <div className=' box-border m-10 md:border-2 p-4 w-70 md:h-80 rounded-lg '>
-                    <ul>
-                        <li className='text-blue-700 font-semibold hover:underline'><Link className=''>Check PNR status</Link></li>
-                        <li className='text-blue-700 font-semibold hover:underline'><Link className=''>Train info</Link></li>
-                        <li className='text-blue-700 font-semibold hover:underline'><Link className=''>Booking list</Link></li>
-                        <li className='text-blue-700 font-semibold hover:underline'><Link className=''>Add Passenger info</Link></li>
-                    </ul>
+                <div className='flex justify-evenly items-center bg-gray-100 h-9/10 rounded-2xl w-1/5 text-center hover:underline'>
+                    <Link to='/my-booking' className={`text-orange-200 ${nav === 'my-booking' && 'text-orange-600' } font-semibold `}>
+                        <FontAwesomeIcon icon={faClipboardList} className="hover:text-4xl text-3xl block md:hidden" /><br />
+                        <span className="hidden md:inline">Booking list</span>
+                    </Link>
                 </div>
             </div>
+
+            <hr className='border w-9/10 mt-4' />
+
+
+            
+
+            <Outlet/>
+            
+            
         </div>
     )
 }
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { setTrainsList} from '../redux/slices/trainSlice';
+import { useDispatch } from 'react-redux';
+
+function Search() {
+    const currentDate = new Date()
+    const min = currentDate.toISOString().split('T')[0]
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const [source, setSource] = useState("");
+    const [destination, setDestination] = useState("");
+    const [sourceSuggestions, setSourceSuggestions] = useState([]);
+    const [destinationSuggestions, setDestinationSuggestions] = useState([]);
+    const [focusedSource,setFocusedSource] = useState(false)
+    const [focusedDestination,setFocusedDestination] = useState(false)
+    const [searchFormData, setSearchFormData] = useState({
+        source:'',
+        destination:'',
+        date:min,
+        classes:'SL'
+    })
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+ 
+    // useEffect(()=>{
+    //     console.log('\n\n\n',searchFormData)
+    //     console.log('sourceSuggestions :',sourceSuggestions)
+    //     console.log('destinationSuggestions :',destinationSuggestions)
+    // },[searchFormData, sourceSuggestions, destinationSuggestions])
+
+    const onFocus = (type) => {
+        setError('')
+        if(type==='source')   setFocusedSource(true)
+        else    setFocusedDestination(true) 
+    }
+    const onBlur = (type) => {
+        setTimeout(() => {
+            if (type === "source") setFocusedSource(false);
+            else setFocusedDestination(false);
+        }, 500); // Delay allows the click event to trigger first
+    }
+
+    const handleOnChange = (e) => {
+        
+        setSearchFormData({...searchFormData, [e.target.name]:e.target.value})
+    }
+
+    const handleSumbit = async (e) => {
+        setError('')
+        
+        e.preventDefault();
+        try{
+            setLoading(true)
+            const res = await fetch(`/api/train/search?sourceStation=${source}&destinationStation=${destination}&date=${searchFormData.date}&trainClass=${searchFormData.classes}`)
+            const data = await res.json()
+            console.log(data)
+            if(data.success)   {
+                dispatch(setTrainsList({
+                    trains:data.trains,
+                    source,
+                    destination,
+                    date: searchFormData.date
+                }))
+                navigate("/train-list", {
+                    state: { data }})}
+            else   setError(data.message)
+            setLoading(false)
+        
+        }catch(e){
+            setError(e.message)
+            setLoading(false)
+        }
+        
+    }
+
+    // Fetch station suggestions
+    const fetchSuggestions = async (query, type) => {
+        if (query==='') {
+            if (type === "source") setSourceSuggestions([]);
+            else setDestinationSuggestions([]);
+            return
+        };
+
+        const response = await fetch(`/api/train/address-list?address=${query}`);
+        const data = await response.json();
+
+        if (type === "source") setSourceSuggestions(data);
+        else setDestinationSuggestions(data);
+    };
+
+    return (
+        <div className='w-9/10 mb-20 '>
+            {/* intro */}
+            <div className='w-full  my-10 text-center'>
+                <h1 className='font-bold text-2xl tracking-[0.2rem]'>
+                    Welcome to Train Ticket Booking center! 🚆
+                </h1>
+            </div>
+            
+            <img src="https://i.etsystatic.com/22260569/r/il/e1c601/3098488926/il_fullxfull.3098488926_7htx.jpg" alt="train cover photo" className='rounded-t-[4rem] object-cover h-50 md:h-100 w-full block mx-auto'/>
+
+            <form onSubmit={(e)=>handleSumbit(e)} className="flex flex-col justify-evenly items-center p-2 mt-2 bg-white shadow-md shadow-gray-300/50 rounded-b-[4rem]">
+                
+                <div className="flex flex-col md:flex-row justify-evenly items-center w-full relative">
+                    {/* From */}
+                    <div className="w-full md:w-1/5">
+                        <label className="text-sm">From</label><br />
+                        <input 
+                            type="text" 
+                            value={source}
+                            name='source'
+                            onChange={(e) => {
+                                setSource(e.target.value);
+                                fetchSuggestions(e.target.value, "source");}}
+                            onFocus={()=>onFocus('source')} onBlur={()=>onBlur('source')}
+                            className="w-full border-2 focus:border-b-4 rounded-lg h-12 text-2xl" 
+                        /> 
+
+                        { focusedSource && 
+                        <ul className='absolute'>
+                            {sourceSuggestions.map((station) => (
+                            <li key={station._id} 
+                                onClick={()=> {
+                                    setSource(station.name);
+                                    setSourceSuggestions([])}}>
+                                {station.name}
+                            </li>
+                            ))}
+                        </ul>}
+                    </div>
+
+                    <span></span>
+
+                    {/* To */}
+                    <div className="w-full md:w-1/5 ">
+                        <label className="text-sm">To</label><br />
+                        <input 
+                            type="text" 
+                            name='destination'
+                            className="w-full border-2 focus:border-b-4 rounded-lg h-12 text-2xl " 
+                            value={destination}
+                            onFocus={()=>onFocus('destination')} 
+                            onBlur={()=>onBlur('destination')}
+                            onChange={(e) => {
+                                setDestination(e.target.value);
+                                fetchSuggestions(e.target.value, "destination");}}/>
+
+                        {focusedDestination &&
+                        <ul className='absolute'>
+                            {destinationSuggestions.map((station) => (
+                            <li key={station._id} 
+                                onClick={()=> { 
+                                    setDestination(station.name);
+                                    setDestinationSuggestions([])}}>
+                                {station.name}
+                            </li>
+                            ))}
+                        </ul>}
+                    </div>
+                    {/* Show suggestions for destination */}
+
+                    {/* Date */}
+                    <div className="w-full md:w-1/5">
+                        <label className="text-sm">Date</label><br />
+                        <input 
+                            type="date"
+                            name='date'
+                            min={min} 
+                            value={searchFormData.date}
+                            onChange={(e)=>handleOnChange(e)}
+                            className="w-full border-2 focus:border-b-4 rounded-lg h-12  text-2xl" 
+                            />
+                    </div>
+
+                    {/* Classess */}
+                    <div className="w-full md:w-1/5">
+                        <label className="text-sm">Classses</label><br />
+                        <select 
+                            type="string" 
+                            name='classes'
+                            value={searchFormData.classes} 
+                            onChange={(e)=>handleOnChange(e)}
+                            className="w-full border-2  focus:border-b-4 rounded-lg h-12  text-2xl"
+                        >
+                            <option value="SL">SLEEPER</option>
+                            <option value="AC2">AC 2 </option>
+                            <option value="AC3">AC 3 </option>
+                        </select>
+                    </div>
+                </div>
+
+                {/* row3 */}
+                <button
+                    type='sumbit'
+                    className={`w-[200px] p-4 m-4 hover:bg-orange-700 text-white font-bold  rounded ${loading ? 'bg-gray-400' : 'bg-orange-500'}`}
+                >{loading ? 'SEARCHING' : 'SEARCH'}</button>
+                <h3 className='text-red-500 font-bold self-center text-2xl'>{error}</h3>
+
+            </form>
+
+            
+        </div>
+        
+    )
+}
+export default Search
