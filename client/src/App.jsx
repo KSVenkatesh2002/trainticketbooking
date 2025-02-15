ec7d142c6ccca1987e7859b86f5113f69f0d7e0d
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-02-15 15:16:21 +0530

    routing changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,12 @@ import './css/App.css';
 import TrainList from './pages/TrainList';
 import Booking from './pages/Booking';
 import Payment from './pages/Payment';
+import TrainInfo from './pages/TrainInfo';
+import PnrStatus from './pages/PnrStatus';
+import BookingList from './pages/BookingList';
+import Search from './pages/Search';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 
 function App() {
@@ -16,15 +22,28 @@ function App() {
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/auth/:types" element={<Auth/>} />
-                <Route element={<PrivateRouter />}>
-                    <Route path="/profile"     element={<Profile/>} />
+                <Route  path="/"                 element={<Home/>} >
+                    <Route  path="/search"           element={<Search/>} />
+                    <Route  path="/train-info"       element={<TrainInfo/>} />
+                    <Route  element={<PrivateRouter />}>
+                        <Route  path="/pnr-status"       element={<PnrStatus/>} />
+                        <Route  path="/my-booking"       element={<BookingList/>} />
+                    </Route>
                 </Route>
-                <Route path="/train-list" element={<TrainList/>} />
-                <Route path='booking' element={<Booking/>} />
-                <Route path='/booking/payment' element={<Payment/>} />
+                <Route  path="/about"            element={<About/>} />
+                <Route  path="/auth"      element={<Auth/>} >
+                    <Route  path="signup"      element={<Signup/>} />
+                    <Route  path="login"      element={<Login/>} />
+                </Route>
+                <Route  path="/train-list"       element={<TrainList/>} />
+
+                <Route  element={<PrivateRouter />}>
+                    <Route  path="/profile"          element={<Profile/>} />
+                    <Route  path='/booking'          element={<Booking/>} />
+                    <Route  path='/booking/payment'  element={<Payment/>} />
+                </Route>
+                
+                
             </Routes>
         </BrowserRouter>
     )
