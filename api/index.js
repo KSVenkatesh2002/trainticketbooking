975a2298fcfc6684bd5a70544997325c229973dd
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-02-17 17:22:59 +0530

    preproduction changes

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import trainRouter from './router/train.router.js'
 import cookieParser from 'cookie-parser';
 import cleanupExpiredBookings from './utils/bookingCleanup.js';
 import deletePendingBookings from './utils/deletePendingBookings.js'
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -22,11 +23,23 @@ mongoose
 cleanupExpiredBookings();
 deletePendingBookings();
 
+const __dirname = path.resolve();
+
 export const app = express();
 
-app.listen(3000, () => { console.log("server is established") });
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.listen(3000, () => { 
+    console.log("server is established") 
+});
+
 app.use(express.json())
 app.use(cookieParser())
+
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/train',trainRouter)
