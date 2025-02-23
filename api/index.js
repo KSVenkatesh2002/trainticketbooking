c1e810aea30a70ecd10912f22f79a5d5db446109
KSVenkatesh2002 <kotavenkatesh2002@gmail.com>
2025-02-23 20:45:32 +0530

    successfully run in local

--- api/index.js
+++ api/index.js
@@ -1,63 +1,56 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './router/user.router.js'
-import authRouter from './router/auth.router.js'
-import trainRouter from './router/train.router.js'
+import userRouter from './router/user.router.js';
+import authRouter from './router/auth.router.js';
+import trainRouter from './router/train.router.js';
 import cookieParser from 'cookie-parser';
 import cleanupExpiredBookings from './utils/bookingCleanup.js';
-import deletePendingBookings from './utils/deletePendingBookings.js'
+import deletePendingBookings from './utils/deletePendingBookings.js';
 import path from 'path';
+
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO, {
-        serverSelectionTimeoutMS: 100000,  // Increase to 100 seconds
-        socketTimeoutMS: 120000,           // Increase to 120 seconds
-        connectTimeoutMS: 120000            // Add connection timeout
+        serverSelectionTimeoutMS: 100000,
+        socketTimeoutMS: 120000,
+        connectTimeoutMS: 120000
     })
     .then(() => {
-        console.log('Connected to MongoDB')
-        // Start Cleanup Job
+        console.log('Connected to MongoDB');
         cleanupExpiredBookings();
         deletePendingBookings();
     })
     .catch((err) => console.log(`Mongo connection error: ${err}`));
 
-
-
 const __dirname = path.resolve();
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.json());
+app.use(cookieParser());
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/train', trainRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-app.listen(3000, () => { 
-    console.log("server is established") 
+const server = app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
 
-// Gracefully release the port on restart
 process.on("SIGTERM", () => {
     console.log("Closing server...");
     server.close(() => process.exit(0));
 });
 
-app.use(express.json())
-app.use(cookieParser())
-
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/train',trainRouter)
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
+    res.status(err.statusCode || 500).json({
         success: false,
-        message,
-        statusCode,
+        message: err.message || 'Internal Server Error'
     });
 });
